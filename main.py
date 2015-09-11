eee8f37dc851e96db2273df1603807014b553f3a
boris_m <boris_m@bk.ru>
2015-09-12 00:54:08 +0300

    add command proc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from messages import Messages
+
 class Settings(object):
     def __init__(self):
         self.dialpan=None
@@ -28,13 +30,71 @@ class PhoneNumberValidater(object):
         number=self.remove_undigit(number)
         return result
 
-    def remove_undigit(self, number):
+    def _remove_undigit(self, number):
         return number
 
+class Tasks(object):
 
+    def test(self):
+        print "just test task"
+
+    def add_record(self):
+        print Messages.add_record
+        user_input = raw_input("input record parameters, please: ")
+        #TODO remove all non alfnum symbols, check encoding
+
+        user_input = user_input.strip("     \t\n\r")
+        user_input = user_input.split(',')
+
+        if len(user_input) < 4:
+            print "you input wrong data, please try again "
+            return None
+        raw_firstname = [user_input[0]]
+        raw_lastname  = [user_input[1]]
+        raw_birthday  = [user_input[2]]
+        raw_phone     = [user_input[3]]
+        #check input is correct
+        print(raw_birthday)
+
+    def remove(self):
+        pass
+
+    def change(self):
+        pass
+
+    def find(self):
+        pass
+
+    def show_all(self):
+        pass
+
+
+
+def execute_task(command):
+    task=Tasks()
+    if command=="add" or command == "a":
+        task.add_record()
+    elif command == "remove" or command == "r":
+        task.remove()
+    elif command == "change" or command == "c":
+        task.remove()
+    elif command == "find" or command == "f":
+        task.find()
+    elif command == "show_all" or command == "s":
+        task.show_all()
+    else:
+        print "no such command ({cmd})\r\n{man}".format(cmd=command, man=Messages.intro)
 def main():
-    pass
+    stop_flag=True
+    print Messages.intro
 
+    try:
+        while stop_flag:
+            user_input = raw_input("input command please: ")
+            execute_task(user_input)
+
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
---
+++ messages.py
@@ -0,0 +1,11 @@
+class Messages():
+    intro="""please input command:
+        add or a      - to add record
+        remove or r   - to remove record
+        find or f     - to find record
+        show_All or s - to show all records
+        change or c   - to change record """
+
+    add_record="""
+    to add records input: first name, last name, birthday (dd/mm/yyyy), phone_number spaced by comma symbol
+    """
