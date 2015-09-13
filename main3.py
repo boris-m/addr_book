7a7f2b23d966d57a56955b7a3580173b23348032
boris_m <boris_m@bk.ru>
2015-09-14 00:23:40 +0300

    save

--- main3.py
+++ main3.py
@@ -11,12 +11,42 @@ class Settings(object):
 
 class Record(object):
     def __init__(self):
+        self.id=None
         self.first_name=None
         self.last_name=None
         self.birthday=None
         self.phone_number=None
         self.legal_phone_number=0 #sqllite have no BOOL so 0 is False
 
+
+    def print_record(self):
+        msg="""
+        id         "{uid}"
+        first name "{fn}"
+        last name  "{ln}"
+        birthday   "{bd}"
+        phone      "{ph}"
+        """.format(
+            uid = self.id,
+            fn  = self.first_name,
+            ln  = self.last_name,
+            bd  = self.birthday,
+            ph  = self.phone_number
+        )
+        print msg
+        return msg
+
+    def fill_record(self,data):
+        try:
+            print len(data)
+            self.id=data[0]
+            self.first_name=data[1]
+            self.last_name=data[2]
+            self.birthday=data[3]
+            self.phone_number=data[4]
+            self.legal_phone_number=data[5]
+        except Exception,e:
+            print "can not parse record:{err}".format(err=e)
 
 class Application(Settings):
     def __init__(self):
@@ -51,6 +81,21 @@ class Application(Settings):
         cur.execute(query)
         print cur.fetchall()
 
+    def __print_record(self,record):
+        msg="""
+        id         "{uid}"
+        first name "{fn}"
+        last name  "{ln}"
+        birthday   "{bd}"
+        phone      "{ph}"
+        """.format(
+            uid = record.id,
+            fn  = record.first_name,
+            ln  = record.last_name,
+            bd  = record.birthday,
+            ph  = record.phone_number
+        )
+
     def __db_add_record(self,record):
         query='INSERT INTO records (id, firstName, lastName ,birthday ,phone, correct_phone) \
         VALUES(NULL, "{fn}", "{ln}" , "{bd}", "{phone}" , "{cor_phone}")'.format(\
@@ -78,13 +123,31 @@ class Application(Settings):
         raw_phone     = [user_input[3]]
         #check input is correct
         print(raw_birthday)
-        
-    def find_record(self,query):
+
+    def __db_find_record(self,query):
         db_query='SELECT * from records where firstName like "%{q}%"'.format(q = query)
         cur=self.db_conection.cursor()
         cur.execute(db_query)
         res=cur.fetchall()
+        print "we found {n} records:".format(n=len(res))
         print res
+
+    def _db_find_record_by_id(self,id):
+        db_query='SELECT * from records where id = "{q}"'.format(q = id)
+        cur=self.db_conection.cursor()
+        cur.execute(db_query)
+        res=cur.fetchall()[0]
+        record=Record()
+        record.fill_record(res)
+        record.print_record()
+
+    def find_record(self):
+        print Messages.find_record
+        user_input = raw_input("input record parameters, please: ")
+        #TODO remove all non alfnum symbols, check encoding
+
+        user_input = user_input.strip("     \t\n\r")
+        self.__db_find_record(user_input)
 
     def set_record(self,id,record):
         query='UPDATE records' \
@@ -96,28 +159,33 @@ class Application(Settings):
         cur.execute(query)
         self.db_conection.commit()
 
-    def remove_record(self,id):
+    def __db_remove_record(self,id):
         id=int(id)
         query='DELETE  FROM records where id={uid}'.format(uid=id)
         cur=self.db_conection.cursor()
         cur.execute(query)
         self.db_conection.commit()
 
+    def remove_record(self):
+        print Messages.remove_record_first
+        self.find_record()
+        user_input = raw_input("please input id of record should be deleted")
+        id=self._db_find_record_by_id(int(user_input))
+
     def execute_task(self,command):
 
         if command=="add" or command == "a":
             self.add_record()
         elif command == "remove" or command == "r":
-            task.remove()
+            self.remove_record()
         elif command == "change" or command == "c":
             task.remove()
         elif command == "find" or command == "f":
-            self.find()
+            self.find_record()
         elif command == "show_all" or command == "s":
             task.show_all()
         else:
             print "no such command ({cmd})\r\n{man}".format(cmd=command, man=Messages.intro)
-
 
     def loop(self):
         user_input = raw_input("input command please: ")
@@ -153,8 +221,9 @@ def test():
     rec.birthday="2003-09-14"
     rec.phone_number="12341243"
     rec.legal_phone_number=0
-    A.find_record("test")
-    #A.run()
+    #A.find_record("test")
+    A.run()
+    #A._db_find_record_by_id(5)
 
 
 if __name__ == "__main__":
--- messages.py
+++ messages.py
@@ -1,11 +1,16 @@
 class Messages():
     intro="""please input command:
         add or a      - to add record
-        remove or r   - to remove record
         find or f     - to find record
         show_All or s - to show all records
-        change or c   - to change record """
+        exit or e     - to exit from app"""
+    #    remove or r   - to remove record
+    #        change or c   - to change record """
+
 
     add_record="""
     to add records input: first name, last name, birthday (dd/mm/yyyy), phone_number spaced by comma symbol
     """
+    find_record="type part of first name and press enter"
+
+    remove_record_first="please find record to delete"
